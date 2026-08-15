1949a6aa2bd77386709b5f3cabd41a744efcd337
rishabh Chauhan <rishabh090106@gmail.com>
2026-08-15 15:00:06 +0000

    datatype

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,36 @@
+"use strict" //treat all js code as newer version
+// alert(3+3)  //we are using nodejs, not browser
+
+console.log(3+
+    3) //code readability should be high
+
+console.log("rishabh")
+
+let name="Rishabh"
+let age=20
+let isLoggedIn=false
+let state;
+
+//number => 2 to power 53
+//bigint
+//sting => ""
+//boolean => true/false
+//null => standalone value
+//undefined 
+//symbol => unique
+
+//object
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof state);
+console.log(typeof isLoggedIn);
+console.log(typeof null);  //object
+console.log(typeof undefined); //undefined
+
+
+
+
+
+
+
