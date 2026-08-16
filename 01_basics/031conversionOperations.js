40c5e4307cbe4e1949ad87c26f0020cd7abebfe2
rishabh Chauhan <rishabh090106@gmail.com>
2026-08-16 14:41:28 +0000

    done

---
+++ 01_basics/031conversionOperations.js
@@ -0,0 +1,36 @@
+let val=3;
+let negval=-val;
+console.log(negval);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**3);
+// console.log(3%2);
+
+let str1="Hello";
+let st2=" Rishabh";
+let st3=str1+st2;
+console.log(st3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+console.log(+true);
+console.log(+"");
+
+let num1,num2,num3;
+num1=num2=num3=2+2;
+console.log(num1,num2,num3);
+
+
+let game=100;
+++game;
+console.log(game);
+
+
+
+
