35d2848e0f1216af5b7cefa09d42b76bf316349e
rishabh Chauhan <rishabh090106@gmail.com>
2026-08-18 21:58:45 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,43 @@
+// const myArr=[1,2,3,4,5,6]
+// console.log(myArr[2]);
+
+// const newArr=["Rishabh","Chauhan","HEllo","World"]
+// const myArr2=new Array(0,1,2,3,4)
+// console.log(myArr2[1]);
+
+// myArr.push(7)
+// myArr.push(8)
+// console.log(myArr);
+// myArr.pop()
+// console.log(myArr);
+
+// myArr.unshift(10)
+// console.log(myArr);
+// myArr.shift()
+// console.log(myArr);
+
+// console.log(myArr.includes(2));
+// console.log(myArr.includes(100));
+// console.log(myArr.indexOf(3));
+// console.log(myArr.indexOf(100));
+
+// const newArr1=myArr.join()
+// console.log(myArr);
+// console.log(newArr1);
+
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+const myArr=[0,1,2,3,4]
+
+console.log("A",myArr);
+console.log(myArr.slice(1,3));
+
+console.log("B",myArr);
+
+console.log(myArr.splice(1,3));
+console.log("C",myArr);
+
+
+
