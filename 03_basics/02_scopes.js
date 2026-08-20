6b7b240ee6509c6019e00e8fb2f78a4dd76349a6
rishabh Chauhan <rishabh090106@gmail.com>
2026-08-20 22:42:42 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -12,3 +12,46 @@ if(true){
 console.log(a);
 // console.log(b);
 // console.log(c)
+
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+function one(){
+    const username="Rishabh"
+    function two(){
+        const website="Yt";
+        console.log(username);
+    }
+    // console.log(website);
+    two();
+    
+}
+one();
+
+
+
+
+if(true){
+    const username="Rishabh"
+    if(username=="Rishabh"){
+        const website="yt"
+        // console.log(username+website);
+        
+    }
+}
+// console.log(username);
+
+
+addOne(3)
+function addOne(num){
+    return num+1;
+}
+
+
+
+// addTwo(5)
+const addTwo=function(num){
+    return num+2
+}
+addTwo(5)
