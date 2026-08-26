f3ec2435862c56a22cbfe0790cb27996180f1f6b
rishabh Chauhan <rishabh090106@gmail.com>
2026-08-26 19:46:32 +0000

    control_flow

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,59 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+// const month=2
+
+// switch(month){
+//     case 1:
+//         console.log("Jan");
+//         break;    
+//     case 2:
+//         console.log("feb");
+//         break;    
+//     case 3:
+//         console.log("mar");
+//         break;    
+//     case 4:
+//         console.log("april");
+//         break;    
+//     case 5:
+//         console.log("may");
+//         break;    
+//     case 6:
+//         console.log("june");
+//         break;    
+//     case 7:
+//         console.log("July");
+//         break;
+//     default:
+//         console.log("This is the month");
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
