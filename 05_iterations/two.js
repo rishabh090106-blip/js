351009f60f83a95f926625515857302bc84d210a
rishabh Chauhan <rishabh090106@gmail.com>
2026-08-26 22:17:36 +0000

    it_two

---
+++ 05_iterations/two.js
@@ -0,0 +1,22 @@
+let i=0;
+while(i<=10){
+    console.log(`value of i = ${i}`);
+    i+=2;
+}
+
+let myarr=["Rishabh","Kohli","Virat"]
+let j=0;
+while(j<myarr.length){
+    console.log(`Value of i = ${myarr[j]}`);
+    j++;
+    
+}
+
+
+
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
