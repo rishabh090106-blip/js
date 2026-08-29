04aaf6097f72240af3f3c81097557deeccc2796c
rishabh Chauhan <rishabh090106@gmail.com>
2026-08-29 08:40:26 +0000

    five

---
+++ 05_iterations/five.js
@@ -0,0 +1,43 @@
+const coding=["js","ruby","java","python","cpp"]
+// coding.forEach(function(item){
+//     console.log(item);
+    
+// })
+
+// coding.forEach((item)=>{
+//     console.log(item);
+    
+// })
+
+
+// function printMe(item){
+//     console.log(item);
+    
+// }
+// coding.forEach(printMe)
+
+
+// coding.forEach((item,ind,arr)=>{
+//     console.log(item,ind,arr);
+    
+// })
+
+
+const myCoding=[
+    {
+        languageName:"Javascript",
+        languagefile:"js"
+    },
+     {
+        languageName:"Java",
+        languagefile:"java"
+    },
+     {
+        languageName:"Python",
+        languagefile:"py"
+    }
+]
+myCoding.forEach((item)=>{
+    console.log(item.languageName);
+    
+})
