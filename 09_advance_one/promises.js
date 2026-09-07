b42348562c96761260591257c23fe3cb92092623
rishabh Chauhan <rishabh090106@gmail.com>
2026-09-07 02:31:29 +0000

    promise

---
+++ 09_advance_one/promises.js
@@ -0,0 +1,11 @@
+const promiseOne=new Promise(function(resolve,reject){
+    setTimeout(function(){
+        console.log("Async task is completed");
+        resolve();
+    },1000)
+})
+
+promiseOne.then(function(){
+    console.log("Promise Consumed");
+
+})
