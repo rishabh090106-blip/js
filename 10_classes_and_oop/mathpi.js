0a78c1d89ce8b6a16289e7e86a3a94e7d4930302
rishabh Chauhan <rishabh090106@gmail.com>
2026-09-08 17:55:01 +0000

    math.pi

---
+++ 10_classes_and_oop/mathpi.js
@@ -0,0 +1,28 @@
+console.log(Math.PI)
+Math.PI=2
+console.log(Math.PI)
+const prop=Object.getOwnPropertyDescriptor(Math,"PI")
+console.log(prop);
+
+const chai={
+    name:"ginger chai",
+    price:250,
+    isAvailable:true
+}
+chai.name="masala chai"
+console.log(chai);
+
+console.log(Object.getOwnPropertyDescriptor(chai,"name"));
+Object.defineProperty(chai,"name",{
+    writable:false,
+    enumerable:false
+})
+console.log(Object.getOwnPropertyDescriptor(chai,"name"));
+
+chai.name="rishabh"
+console.log(chai);
+for(let [key,value] of Object.entries(chai)){
+    console.log(`${key}:${value}`);
+    
+}
+
